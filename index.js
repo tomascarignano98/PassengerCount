acd25489b6bae221b9247b7e2cb1f5eed9015bc5
Tomas Carignano <tomascarignano98@gmail.com>
2021-07-12 20:04:36 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,16 @@
+let count = 0;
+const countEl = document.querySelector("#count-el");
+
+function increment() {
+  count++;
+  countEl.textContent = count;
+}
+
+const saveEl = document.querySelector("#save-el");
+function save() {
+  const entry = ` ${count} -`;
+  saveEl.textContent += entry;
+
+  countEl.textContent = count;
+  count = 0;
+}
